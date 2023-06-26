28091b946c706d0f0401008304f148a2ca96cc85
Hamza Bouchdouk <130603919+Hamzabobo@users.noreply.github.com>
2023-06-26 18:36:49 -0400

    Add files via upload

---
+++ project2/assets/js/script.js
@@ -0,0 +1,20 @@
+$('#inquirysubmit').click(function(){
+                          
+      console.log('click'); 
+      namevalue= $('#nm').val();
+      emailvalue= $('#em').val();
+    console.log(namevalue,emailvalue);
+    $('#nmval').html(namevalue);
+	$('#emval').html(emailvalue);
+   
+    
+     
+                                             
+                          
+     });
+
+
+
+		
+
+	
